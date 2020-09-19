1d8d1e01c76daf6486e75141ac51fb123c4ade44
David Felix <david.felix02@hotmail.com>
2020-09-19 03:41:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,73 +1,94 @@
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-
-  });
-  
 let addToy = false;
-
 let toyCollection = document.querySelector("#toy-collection");
-
 let newToyName = document.querySelector(".add-toy-form")
 
-  fetch("http://localhost:3000/toys")
-    .then(res => res.json())
-    .then(toyname => createNewToy(toyname))
-    let createNewToy = (toyname) =>{ 
-     toyname.forEach(toyObj => {
-       toyObj.forEach(createNewToyName)});
-    }
-    newToyName.addEventListener("submit", function(evt) {
-      evt.preventDefault()
-      const newToyName = evt.target.name.value
-      const newToyUrl = evt.target.image.value
-      fetch("http://localhost:3000/toys", {
-        method: "POST",
+let createNewToyName = (toy) => {
+
+    let toyDiv = document.createElement("div")
+    toyDiv.className = "card"
+  
+    let toyH2 = document.createElement("h2")
+    toyH2.innerText = toy.name
+  
+    let toyImg = document.createElement("img")
+    toyImg.src= toy.image
+    toyImg.className = "toy-avatar"
+  
+    let toyP = document.createElement("p")
+    toyP.innerText = `${toy.likes} likes` 
+  
+    let toyButton = document.createElement("button")
+    toyButton.innerText = "Like"
+    toyButton.className = "link-btn"
+  
+    toyDiv.append(toyH2, toyImg, toyP, toyButton)
+    toyCollection.append(toyDiv)
+  
+    toyButton.addEventListener("click", (ev) => {
+      let newLikes = toy.likes + 1
+      
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: "PATCH",
         headers: 
         {
           "Content-Type": "application/json",
           Accept: "application/json"
         },
         body: JSON.stringify({
-          "name": newToyName,
-          "image": newToyUrl,
-          "likes": 0
+          "likes": newLikes
         })
-      }) 
-      .then(res => res.json())
-      .then(newToyName => createNewToyName(newToyName))
-
-      let createNewToyName = (newToyName) => {
-
-        let toyDiv = document.createElement("div")
-        toyDiv.className = "card"
-        let toyH2 = document.createElement("h2")
-        toyH2.innerText = newToyName.name
-        toyDiv.append(toyH2)
-        let toyImg = document.createElement("img")
-        toyImg.src= newToyName.image
-        toyImg.className = "toy-avatar"
-        toyDiv.append(toyImg)
-        let toyP = document.createElement("p")
-        toyP.innerText = `${newToyName.likes} likes` 
-        toyDiv.append(toyP)
-        let toyButton = document.createElement("button")
-        toyButton.innerText = "Like"
-        toyButton.className = "link-btn"
-        toyDiv.append(toyButton)
-        toyCollection.append(toyDiv)
-      }
+      })
+        .then(res => res.json())
+        .then((newToyObj) => {
+          toyP.innerText = `${newToyObj.likes} Likes`
+  
+          toy.likes = newToyObj.likes
+        })
     })
-    
+}
 
-})
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
+addBtn.addEventListener("click", () => {
+  // hide & seek with the form
+  addToy = !addToy;
+  if (addToy) {
+    toyFormContainer.style.display = "block";
+  } else {
+    toyFormContainer.style.display = "none";
+  }
+
+});
+
+fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(toys => {
+    toys.forEach((toy) => {createNewToyName(toy)})
+  });
+
+  newToyName.addEventListener("submit", function(evt) {
+    evt.preventDefault()
+    let newToyName = evt.target.name.value
+    let newToyUrl = evt.target.image.value
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: 
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": newToyName,
+        "image": newToyUrl,
+        "likes": 0
+      })
+    }) 
+    .then(res => res.json())
+    .then(newToy => {
+      createNewToyName(newToy)
+      evt.target.reset()
+    })
+  })
+
+  
+
